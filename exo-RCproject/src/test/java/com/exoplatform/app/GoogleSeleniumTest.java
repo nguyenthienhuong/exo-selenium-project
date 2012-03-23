7dd52ac938889e8e73324b00259830dbb1fac0d6
NGUYEN Thien Huong <nguyenthienhuong0305@gmail.com>
2012-03-23 18:45:32 +0100

    Commit the first example to RC project with Selenium

---
+++ exo-RCproject/src/test/java/com/exoplatform/app/GoogleSeleniumTest.java
@@ -0,0 +1,47 @@
+package com.exoplatform.app;
+
+import com.thoughtworks.selenium.HttpCommandProcessor;
+import com.thoughtworks.selenium.Selenium;
+import com.thoughtworks.selenium.DefaultSelenium;
+
+public class GoogleSeleniumTest {
+    static Selenium browser;
+
+    public static void main(String[] arguments) {
+        HttpCommandProcessor cmdProcessor =
+                new HttpCommandProcessor("localhost", 4444, "*firefox", "http://www.google.com");
+        browser = new DefaultSelenium(cmdProcessor);
+        browser.start();
+        browser.open("/");
+
+        boolean result;
+        try {
+            result = searchTest();
+        } catch(Exception e) {
+            e.printStackTrace();
+            result = false;
+        } finally {
+            browser.stop();
+        }
+
+        System.out.println("Test " + (result? "passed." : "failed."));
+        if (!result) {
+            System.exit(1);
+        }
+    }
+
+    private static boolean searchTest() {
+        browser.type("q", "qa automation");
+        browser.mouseDown("btnG");
+        browser.mouseUp("btnG");
+        browser.waitForCondition("selenium.browserbot.getCurrentWindow().document.getElementById('body') === null", "30000");
+
+        // sleep so that you get time to see the effect of above steps
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            //
+        }
+        return browser.isTextPresent("About * results");
+    }
+}
